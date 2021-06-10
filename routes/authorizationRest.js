67d2bd924bac48c6b8d31da6ae0fa282c8b61886
Dmitriy Mekh <codemekh@gmail.com>
2021-06-10 11:28:17 +0300

    Changed hashing algorithm, added secret key as salt and as hash key, cleaned whitelist

--- routes/authorizationRest.js
+++ routes/authorizationRest.js
@@ -6,7 +6,7 @@ const router = express.Router()
 // Get your token by login and password
 router.post('/generateToken', (req, res, next) => {
   const { login, password } = req.body
-  res.json({ status: 'ok', id: hasher.getTokenByLoginAndPassword(login, password) })
+  res.json({ status: 'ok', token: hasher.getTokenByLoginAndPassword(login, password) })
 })
 
 module.exports = router
--- utils/hasher.js
+++ utils/hasher.js
@@ -4,7 +4,7 @@ import config from '../core/config'
 const hasher = {}
 
 hasher.getTokenByLoginAndPassword = (login, password) => {
-  return crypto.AES.encrypt(login + password, config.SECRET_KEY).toString()
+  return crypto.HmacSHA3(JSON.stringify([login, config.SECRET_KEY, password]), config.SECRET_KEY).toString()
 }
 
 export default hasher
